92920758425e947a87abd4d8df05dfacebe9dc8f
Sorosliu1029 <soros.liu1029@gmail.com>
2016-01-08 00:59:53 +0800

    modify Python Challenge 3

--- PythonChallenge3.py
+++ PythonChallenge3.py
@@ -7,7 +7,7 @@ from urllib import request
 import re
 with request.urlopen('http://www.pythonchallenge.com/pc/def/equality.html') as f:
     data = f.read().decode('utf-8')
-    data = ''.join(data.split('\n'))
-    content = re.findall(r'(<!\-\-.*?\-\->)', data)[0].strip('<!-->')
-    small_letters = re.findall(r'[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]', content)
-    print(''.join(small_letters))
+data = ''.join(data.split('\n'))
+content = re.findall(r'(<!\-\-.*?\-\->)', data)[0].strip('<!-->')
+small_letters = re.findall(r'[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]', content)
+print(''.join(small_letters))
