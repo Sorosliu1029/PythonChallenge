095044c5c35cbd35b190272fa75f4fb2a9dd87fd
Sorosliu1029 <soros.liu1029@gmail.com>
2016-01-08 01:00:18 +0800

    modify Python Challenge 2

--- PythonChallenge2.py
+++ PythonChallenge2.py
@@ -8,9 +8,9 @@ from urllib import request
 import re
 with request.urlopen('http://www.pythonchallenge.com/pc/def/ocr.html') as f:
     data = f.read().decode('utf-8')
-    data = ''.join(data.split('\n'))
-    content = re.findall(r'(<!\-\-.*?\-\->)', data)[1].strip('<!-->')
-    counter = {}
-    for char in content:
-        counter[char] = counter.get(char, 0) + 1
-    print(''.join([char for char in content if counter[char] == 1]))
+data = ''.join(data.split('\n'))
+content = re.findall(r'(<!\-\-.*?\-\->)', data)[1].strip('<!-->')
+counter = {}
+for char in content:
+    counter[char] = counter.get(char, 0) + 1
+print(''.join([char for char in content if counter[char] == 1]))
